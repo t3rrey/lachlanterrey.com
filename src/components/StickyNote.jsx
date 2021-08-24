22692ebb6117887d420596aaafc0196e1240838a
Lachlan <lachlanterrey@yahoo.com>
2021-08-24 20:33:42 +1000

    Updated taskbar icons

---
+++ src/components/StickyNote.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//https://jsfiddle.net/Logan_Wayne/eovLdrao/
+
+const StickyNote = () => {
+  return (
+    <div className="main-sticky-wrapper">
+      <div className="sticky-menu">
+        <button></button>
+      </div>
+      <div className="sticky-content"></div>
+    </div>
+  );
+};
+
+export default StickyNote;
--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import "../styles/taskbar.css";
 import TaskbarIcon from "./TaskbarIcon";
 import personIcon from "../assets/icons/personIcon.svg";
+import calculatorIcon from "../assets/icons/calculatorIcon.svg";
+import mapIcon from "../assets/icons/mapIcon.svg";
+import stickyNoteIcon from "../assets/icons/stickyNoteIcon.svg";
+import settingsIcon from "../assets/icons/settingsIcon.svg";
 
 const Taskbar = () => {
   return (
     <div className="main-taskbar-wrapper">
       <div className="taskbar">
         <TaskbarIcon image={personIcon} />
-        <TaskbarIcon image={personIcon} />
-        <TaskbarIcon image={personIcon} />
-        <TaskbarIcon image={personIcon} />
-        <TaskbarIcon image={personIcon} />
-        <TaskbarIcon image={personIcon} />
+        <TaskbarIcon image={calculatorIcon} />
+        <TaskbarIcon image={mapIcon} />
+        <TaskbarIcon image={stickyNoteIcon} />
+        <TaskbarIcon image={settingsIcon} />
       </div>
     </div>
   );
