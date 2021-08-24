dc77d15a80ac8444b79faf7cd192eff638b68aed
Lachlan <lachlanterrey@yahoo.com>
2021-08-25 00:05:13 +1000

    upddate styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Taskbar from "./components/Taskbar";
 import "./styles/App.css";
 import background from "./assets/background.svg";
+import StickyNote from "./components/StickyNote";
 
 function App() {
   return (
     <div className="App">
       <img className="background" src={background} alt="" />
       <Taskbar />
+      <StickyNote />
     </div>
   );
 }
--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import "../styles/stickynote.css";
+import crossIcon from "../assets/icons/crossIcon.svg";
+import plusIcon from "../assets/icons/plusIcon.svg";
 
 //https://jsfiddle.net/Logan_Wayne/eovLdrao/
+
+//https://codepen.io/ZeroX-DG/pen/vjdoYe
+
+//https://htmldom.dev/make-a-resizable-element/
 
 const StickyNote = () => {
   return (
     <div className="main-sticky-wrapper">
       <div className="sticky-menu">
-        <button></button>
+        <img className="menu-icon" src={crossIcon} alt="" />
+        <img className="menu-icon" src={plusIcon} alt="" />
       </div>
       <div className="sticky-content"></div>
     </div>
