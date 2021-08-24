5e35c89eda04f8a29cadbea12ee8aaff444acb4d
Lachlan <lachlanterrey@yahoo.com>
2021-08-24 19:58:39 +1000

    Updated Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Taskbar from "./components/Taskbar";
 import "./styles/App.css";
+import background from "./assets/background.svg";
 
 function App() {
   return (
     <div className="App">
+      <img className="background" src={background} alt="" />
       <Taskbar />
     </div>
   );
