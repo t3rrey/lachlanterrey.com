4f2a396f95d7ca4714cb14cad144208b898cd05d
Lachlan <lachlanterrey@yahoo.com>
2022-03-22 13:04:40 +1100

    Application State now using context api

--- src/context/OpenApplicationsContext.jsx
+++ src/context/OpenApplicationsContext.jsx
@@ -1,12 +1,20 @@
 import { createContext, useState, useEffect } from "react";
-import axios from "axios";
 
 const Context = createContext();
 
 export function Provider(props) {
-  
-
-  return <Context.Provider />;
+  const [openApplications, setOpenApplications] = useState([
+    {
+      name: "stickyNotes",
+      width: 500,
+      height: 500,
+      sessions: 1,
+      maximised: false,
+      minimised: false,
+      titleBarStyle: "#ffffff",
+    },
+  ]);
+  return <Context.Provider value={openApplications} {...props} />;
 }
 
 export default Context;
