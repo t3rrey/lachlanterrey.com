b953366c80bf3bab7a0b1dad0a24694a042ecc6a
Lachlan <lachlanterrey@yahoo.com>
2022-03-26 12:34:53 +1100

    Added Slate

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import styled from "styled-components";
+
+const Ewrap = styled.div`
+  background: '#ffffff;
+`;
+const editor = new EditorJS({
+  /**
+   * Id of Element that should contain the Editor
+   */
+  holder: "editorjs",
+
+  /**
+   * Available Tools list.
+   * Pass Tool's class or Settings object for each Tool you want to use
+   */
+  tools: {
+    header: Header,
+    list: List,
+  },
+});
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return <div id="editorjs">Editor</div>;
 };
 
 export default Editor;
